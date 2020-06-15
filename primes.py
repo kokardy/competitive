cfcd1c399fa106ac7d40d9d3b521ae1b88e67030
kokardy <mgc1028@gmail.com>
2020-06-15 19:52:02 +0900

    エラトステネスのふるいの実装

--- primes.py
+++ primes.py
@@ -1,5 +1,5 @@
 
-def primes(n):
+def primes2(n):
     yield 2
     import numpy as np
     nn = (n // 3) + 1
@@ -11,15 +11,43 @@ def primes(n):
     for p in range(3, n+1, 2):
         if p not in notprimes:
             yield p
-    
+
+def primes(n):
+    import numpy as np
+    numbers = np.full(n, True, dtype=np.bool)
+
+    numbers[0:n] = True
+    numbers[0] = False
+    numbers[1] = False
+
+    for k in range(1, n):
+        if not numbers[k]:
+            continue
+        if numbers[k]:
+            yield k
+        i = 1
+        while k * i < n: 
+            ki = k*i
+            numbers[ki] = False
+            i += 1
+
 def prime_test(n):
     print("prime:", end="")
     for i in primes(n):
         print(f"{i}", end=", ") 
     print("")
+        
+    
+def prime_test2(n):
+    for i, j in zip(primes(n), primes2(n)):
+        if i != j:
+            raise Exception(f"i={i} j={j}")
+    print("PASS TEST")
 
 def test():
     prime_test(1000)
+    prime_test2(1000)
+    
 
 if __name__ == '__main__':
     test()
