614c56425584071ed17248886bfda7358fbb6900
kokardy <2720900+kokardy@users.noreply.github.com>
2024-12-07 17:21:52 +0900

    typehint

--- python/src/BIT.py
+++ python/src/BIT.py
@@ -3,24 +3,46 @@
 from typing import Callable
 
 
-class BIT:
+class BIT[T]:
     """Binary Indexed Tree"""
 
-    def __init__(self, n: int, operator: Callable[[int, int], int], zero):
-        """Initialize"""
+    def __init__(self, n: int, operator: Callable[[T, T], T], zero: T) -> None:
+        """
+        Initialize
+
+        Args:
+        ----
+            n (int): Number of elements
+            operator (Callable[[int, int], int]): Operator
+            zero(T): Zero element
+        """
         self.n = n
         self.operator = operator
         self.zero = zero
-        self.tree = [zero] * (n + 1)
+        self.tree = [zero for _ in range(n + 1)]
 
-    def update(self, i: int, x: int) -> None:
-        """Update"""
+    def update(self, i: int, x: T) -> None:
+        """
+        Update
+
+        Args:
+        ----
+            i (int): Index
+            x (T): Value
+
+        """
         while i <= self.n:
             self.tree[i] = self.operator(self.tree[i], x)
             i += i & -i
 
-    def value(self, i: int) -> int:
-        """Return value"""
+    def value(self, i: int) -> T:
+        """
+        Return value
+
+        Args:
+        ----
+            i (int): Index
+        """
         s = self.zero
         while i > 0:
             s = self.operator(s, self.tree[i])
